9defb1aa068551cacd92079591026c6fab9f7515
BICHRI <modoumbene.diouf@uadb.edu.sn>
2019-07-04 16:53:45 +0200

    Initial commit

---
+++ app/src/main/java/com/example/bichri/diaayema/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.bichri.diaayema;
+import  androidx.annotation.RequiresApi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Button registerButton,loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        registerButton = findViewById(R.id.main_register_btn);
+        loginButton = findViewById(R.id.main_login_btn);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
