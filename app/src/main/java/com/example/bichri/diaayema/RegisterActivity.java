062c056452da8a82d0b5edcecb2341ee52c729c4
BICHRI <modoumbene.diouf@uadb.edu.sn>
2019-07-04 19:22:28 +0200

    Creation du register

--- app/src/main/java/com/example/bichri/diaayema/RegisterActivity.java
+++ app/src/main/java/com/example/bichri/diaayema/RegisterActivity.java
@@ -1,14 +1,26 @@
 package com.example.bichri.diaayema;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText inputName,inputPhone,inputPassword;
     private Button createAccountButton;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +30,61 @@ public class RegisterActivity extends AppCompatActivity {
         inputName = findViewById(R.id.register_name_input);
         inputPhone = findViewById(R.id.register_name_input);
         createAccountButton = findViewById(R.id.register_btn);
+        loadingBar = new ProgressDialog(this);
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateAccount();
+            }
+        });
     }
+
+    private void CreateAccount() {
+        String name = inputName.getText().toString();
+        String phone = inputPhone.getText().toString();
+        String password = inputPassword.getText().toString();
+
+        if(TextUtils.isEmpty(name)){
+            Toast.makeText(this, "Veillez remplire tous les champs", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(phone)){
+            Toast.makeText(this, "Veillez remplire tous les champs", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Veillez remplire tous les champs", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            loadingBar.setTitle("Creation du compte");
+            loadingBar.setMessage("Veillez patienter un instant");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+            ValidatePhoneNumber(name,phone,password);
+
+        }
+    }
+
+    private void ValidatePhoneNumber(String name, final String phone, String password) {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (!(dataSnapshot.child("Users").child(phone).exists())){
+                    
+                }
+                else {
+                    Toast.makeText(RegisterActivity.this, "Ce numéro de telephone exite déja", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
 }
