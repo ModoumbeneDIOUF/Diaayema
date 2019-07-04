c6623f458de05e887abc8a53b6368e98470d487b
BICHRI <modoumbene.diouf@uadb.edu.sn>
2019-07-04 17:40:33 +0200

    Creation du register

--- app/src/main/java/com/example/bichri/diaayema/RegisterActivity.java
+++ app/src/main/java/com/example/bichri/diaayema/RegisterActivity.java
@@ -3,12 +3,20 @@ package com.example.bichri.diaayema;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText inputName,inputPhone,inputPassword;
+    private Button createAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        inputName = findViewById(R.id.register_name_input);
+        inputPhone = findViewById(R.id.register_name_input);
+        createAccountButton = findViewById(R.id.register_btn);
     }
 }
